5a22cc2ac354a3db8c52591b41c39a6c779ed59e
marv <manchkhava.bozheva@gmail.com>
2025-10-20 21:43:46 +0300

    maqfer: created MongoDB  schemes

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 6
+  },
+  avatarUrl: {
+    type: String,
+    default: ''
+  },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student'
+  }
+}, {
+  timestamps: true // автоматическое добавление createdAt и updatedAt
+});
+
+// шифрование перед сохр.
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// проверка
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+module.exports = mongoose.model('User', userSchema);
