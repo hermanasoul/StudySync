34d8e384cbdca61cc9ca4865288a3469bdc9bce6
marv <manchkhava.bozheva@gmail.com>
2025-10-20 22:47:15 +0300

    move api

---
+++ client/src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const authAPI = {
+  register: (userData) => api.post('/auth/register', userData),
+  login: (credentials) => api.post('/auth/login', credentials),
+};
+
+export const subjectsAPI = {
+  getAll: () => api.get('/subjects'),
+  getById: (id) => api.get(`/subjects/${id}`),
+};
+
+export default api;
