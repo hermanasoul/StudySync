ec102cebec9bfe5be7c20bb82431a34ab20793ce
marv <manchkhava.bozheva@gmail.com>
2025-10-20 22:56:27 +0300

    upd

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
       <Header />
       
-      {/* Герой секция */}
+      {/* ???Герой секция */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6">
