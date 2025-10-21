95c2c11679ef810e91e28bec446b4cfdf9926e91
marv <manchkhava.bozheva@gmail.com>
2025-10-21 19:42:57 +0300

    update -v

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -15,12 +16,27 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
+            {/* Публичные маршруты */}
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/subjects/:subjectId" element={<NotesPage />} />
-            <Route path="/subjects/:subjectId/flashcards" element={<FlashcardsPage />} />
+            
+            {/* Защищенные маршруты */}
+            <Route path="/dashboard" element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/subjects/:subjectId" element={
+              <ProtectedRoute>
+                <NotesPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/subjects/:subjectId/flashcards" element={
+              <ProtectedRoute>
+                <FlashcardsPage />
+              </ProtectedRoute>
+            } />
           </Routes>
         </div>
       </Router>
